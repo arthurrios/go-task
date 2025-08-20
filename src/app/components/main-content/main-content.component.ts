42f98eb92c4a1850ec96e245198fa1ce25ceb5b3
arthur <arthur.rios007@gmail.com>
2025-08-20 14:40:43 -0300

    feat: add task list section

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { WelcomeSessionComponent } from "../welcome-session/welcome-session.component";
+import { TaskListSectionComponent } from "../task-list-section/task-list-section.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSessionComponent],
+  imports: [WelcomeSessionComponent, TaskListSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
