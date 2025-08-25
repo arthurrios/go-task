648669e6a4b14152e001f51692e0d9f179e1e512
arthur <arthur.rios007@gmail.com>
2025-08-25 09:45:33 -0300

    feat: create new task

--- src/app/components/welcome-session/welcome-session.component.ts
+++ src/app/components/welcome-session/welcome-session.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ModalControllerService } from '../../services/modal-controller.service';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-welcome-session',
@@ -9,12 +10,16 @@ import { ModalControllerService } from '../../services/modal-controller.service'
 })
 export class WelcomeSessionComponent {
   private readonly _modalControllerService = inject(ModalControllerService)
+  private readonly _taskService = inject(TaskService)
 
   openNewTaskModal() {
     const dialogRef = this._modalControllerService.openNewTaskModal()
 
     dialogRef.closed.subscribe((taskForm) => {
       console.log('Created task: ', taskForm)
+      if (taskForm) {
+        this._taskService.addTask(taskForm)
+      }
     })
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
+import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
+import { TaskStatusEnum } from '../enums/task-status.enum';
+import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamp';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +23,16 @@ export class TaskService {
   readonly doneTasks = this.doneTasks$.asObservable()
 
   // Add a new task
+  addTask(taskInfos: ITaskFormControls) {
+    const newTask: ITask = {
+      ...taskInfos,
+      status: TaskStatusEnum.TODO,
+      id: generateUniqueIdWithTimestamp(),
+      comments: []
+    }
+
+    const currentList = this.todoTasks$.value
+
+    this.todoTasks$.next([...currentList, newTask])
+  }
 }
